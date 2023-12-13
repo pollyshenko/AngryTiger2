e64293b28bc43c366a13945490bd3165ba0d93da
pollyshenko <pollyshenko@gmail.com>
2023-12-13 18:15:09 +0400

    function Calculator# switch/case научилась передавать добл в стринг

--- src/Calculator.java
+++ src/Calculator.java
@@ -8,47 +8,60 @@ public class Calculator {
         Scanner scan = new Scanner(System.in);
         System.out.println("Это калькулятор.\nОн должен получить строку в формате <первый аргумент> <операция> <второй аргумент> разделенными пробелом.\nВведите выражение: ");
         String mainString = scan.nextLine();
-        System.out.println(calculate(mainString));
+        System.out.println("resultSS "+calculate(mainString));
     }
     public static List<String> calculate(String input) {
         String[] parts = input.split(" ");
-        List<String> result = new ArrayList<>();
+        List<String> resultS = new ArrayList<>();
+        List<Double> resultD = new ArrayList<>();
         for(int i = 0; i < parts.length; ++i) {
             String typePart = "String";
             try{
-                long fromString = Long.parseLong(parts[i]);
+                double fromString = Long.parseLong(parts[i]);
+                resultD.add(fromString);
                 typePart = "Integer";
             }
             catch (Exception e) {
                 System.out.println("Ошибка: " + parts[i] + " нельзя перевести в число!");
             }
-            //System.out.println(i + " "+ parts.length );
             switch (typePart) {
                 case "String":
-                    result.add(" строка - " + parts[i]);
-                    System.out.println("String - " + typePart);
+                    resultS.add("String");
+                    System.out.println("string - " + typePart);
                     break;
                 case "Integer":
-                    result.add(" числа - " + parts[i]);
-                    System.out.println("Integer - " + typePart);
+                    resultS.add("Integer");
+                    System.out.println("integer - " + typePart);
                     break;
                 default:
                     throw new IllegalStateException("Unexpected value: " + parts[i]);
             }
         }
-        return result;
+        if (resultS.get(0) == resultS.get(1) && resultS.get(0) == "Integer") {
+            double resultDD = calculate(resultD.get(0), resultD.get(1), "+");
+            //System.out.println("Integer" + resultD.get(0) );
+            String fromLong = Long.toString((long) resultDD);
+            List<String> dResultS = new ArrayList<>();
+            dResultS.add(fromLong);
+            return dResultS;
+        }
+        else {
+          System.out.println(resultD + " - resultD");
+          return resultS;
+        }
 
         //TODO напишите метод Calculate, который будет получать строку в формате <первый аргумент> <операция> <второй аргумент>
         //разделенными пробелом и отправлять в нужные методы
+    }
+   private static double calculate(double a, double b, String operation) {
+        double resultDouble = 666;
+        return resultDouble;
+        //TODO напишите метод для работы с числами со следующими операциями: +, -, /, *, %
     }
 /*
     private static String calculate(String a, String b, String operation) {
         //TODO напишите метод для складывания, либо вычитания строк
         //Важно: для вычитания строки, они должны совпадать по регистру
-    }
-
-    private static double calculate(double a, double b, String operation) {
-        //TODO напишите метод для работы с числами со следующими операциями: +, -, /, *, %
     }
 
     public static int getNumbers(int a, int b) {
