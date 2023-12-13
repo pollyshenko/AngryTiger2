6ba61ce4e7470113580a9d286e74da420d31b944
pollyshenko <pollyshenko@gmail.com>
2023-12-13 16:20:10 +0400

    function Calculator# add switch/case

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Calculator {
@@ -5,14 +7,33 @@ public class Calculator {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Это калькулятор.\nОн должен получить строку в формате <первый аргумент> <операция> <второй аргумент> разделенными пробелом.\nВведите выражение: ");
-
+        String mainString = scan.nextLine();
+        System.out.println(calculate(mainString));
     }
-    /* public static String calculate(String input) {
+    public static List<String> calculate(String input) {
+        String[] parts = input.split(" ");
+        List<String> result = new ArrayList<>();
+        for(int i = 0; i < parts.length; ++i) {
+            System.out.println(i + " "+ parts.length );
+            switch (i) {
+                case 0:
+                    result.add(" первое слово - " + parts[i]);
+                    System.out.println("case 0 - " + i);
+                    break;
+                case 1:
+                    result.add(" второе слово - " + parts[i]);
+                    System.out.println("case 1 - " + i);
+                    break;
+                default:
+                    throw new IllegalStateException("Unexpected value: " + parts[i]);
+            }
+        }
+        return result;
 
         //TODO напишите метод Calculate, который будет получать строку в формате <первый аргумент> <операция> <второй аргумент>
         //разделенными пробелом и отправлять в нужные методы
     }
-
+/*
     private static String calculate(String a, String b, String operation) {
         //TODO напишите метод для складывания, либо вычитания строк
         //Важно: для вычитания строки, они должны совпадать по регистру
