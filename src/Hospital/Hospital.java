b9a5f71b11c0911d472a31d91309ff782e5807ae
pollyshenko <pollyshenko@gmail.com>
2023-12-28 20:33:31 +0400

    function Hospital# создано 2 метода

--- src/Hospital/Hospital.java
+++ src/Hospital/Hospital.java
@@ -1,25 +1,71 @@
 package Hospital;
 
+//import java.text.DecimalFormat;
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+import java.util.Random;
+
 public class Hospital {
+    int patientsCount;
+    //float[] patientsTemperatures;
     public Hospital(int patientsCount) {
+        if (patientsCount <= 0)
+            throw new IllegalArgumentException("Число должно быть положительным");
+        this.patientsCount = patientsCount;
+       // this.patientsTemperatures = generatePatientsTemperatures();
+
+        //Это конструктор
         //TODO создание больницы с указанным кол-вом пациентов
         // если значение не верное, бросать IllegalArgumentException с помощью метода throw new <Exception.class>
     }
-/*
+
     //нужно кол-во пациентов
-    public float[] generatePatientsTemperatures() {
+    public float[] generatePatientsTemperatures(int patientsCount) {
+
+        //Класс Random. Метод random()
+        //DecimalFormat decimalFormat = new DecimalFormat( "#.#" ); - на выходе строка, что не подходит
+        //double scale = Math.pow(10, 1); - не очень корректно округляет в итоге
+        //  patientsTemperatures[i] = (float) (Math.ceil(number * scale) / scale);
+
+        MathContext context = new MathContext(3, RoundingMode.HALF_UP);
+        Random rand = new Random();
+        int a=32;
+        int b=40;
+        float[] patientsTemperatures = new float[patientsCount];
+        for(int i = 0; i<patientsCount; i++){
+            float number = a + rand.nextFloat(b - a);
+            System.out.println(number);
+            BigDecimal result = new BigDecimal(number, context);
+            patientsTemperatures[i] = result.floatValue();
+        }
+
         //TODO реализовать генерацию массива температур от 32 до 40 градусов
-        return new float[0];
+        /*
+        Класс Math. Метод random()
+        int a=10;
+        int b=20;
+        int x= a + (int)(Math.random() * ((b - a) + 1));
+        System.out.println("Случайное число x: " + x);
+         */
+        return patientsTemperatures;
     }
 
     //нужна генерация температур
-    public int getCountHealthy() {
+    public int getCountHealthy( float [] patientsTemperatures) {
+
+        int countHealthy = 0;
+        for(float i:patientsTemperatures){
+            if(i < 36.9 && i > 36.2){
+                countHealthy++;
+            }
+        }
         //TODO реализовать подсчёт кол-ва здоровых(36,2 < x < 36,9) в сгенерированном массиве температур
-        // если метод уже был вызван ранее, не выполнять подсчет снова, а возращать текущее вычисленное значение
+        // если метод уже был вызван ранее, не выполнять подсчет снова, а возвращать текущее вычисленное значение
         // если генерация температур не была выполнена, вызвать метод генерации
-
+       return countHealthy;
     }
-
+/*
     //нужна генерация температур
     public String getTemperaturesToString() {
         //TODO реализовать получение массива температур в виде строки <double>, <double>, <double>
--- src/Hospital/Main.java
+++ src/Hospital/Main.java
@@ -1,11 +1,21 @@
 package Hospital;
 
-public class Main {
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class Main {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int patientsCount = scan.nextInt();
+
+        Hospital first = new Hospital(patientsCount);
+        float [] primer = first.generatePatientsTemperatures(patientsCount);
+        int countHealthy = first.getCountHealthy(primer);
         //TODO напишите консольное приложение для работы с классом больница, учитывая, что сгенерированные и вычисленные переменные,
         // нельзя будет менять.
-        System.out.printf("");
+        System.out.println(first.patientsCount);
+        System.out.println(Arrays.toString(primer));
+        System.out.println("здоровых - " + countHealthy);
         ;
     }
 }
